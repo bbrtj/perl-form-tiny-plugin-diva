32b8bace0d8d452e3035c84351035c6bc2b1eea6
bbrtj <brtastic.dev@gmail.com>
2022-01-22 19:01:48 +0100

    Document the plugin

--- lib/Form/Tiny/Plugin/Diva.pm
+++ lib/Form/Tiny/Plugin/Diva.pm
@@ -76,45 +76,144 @@ __END__
 
 =head1 NAME
 
-Form::Tiny::Plugin::Diva - Perl extension for blah blah blah
+Form::Tiny::Plugin::Diva - Form::Diva integration for Form::Tiny
 
 =head1 SYNOPSIS
 
- use Form::Tiny::Plugin::Diva;
- blah blah blah
+	### Form configuration
+
+	use Form::Tiny plugins => ['Diva'];
+
+	# Form::Diva configuration can be passed using diva_config
+	diva_config label_class => 'my-label-class';
+
+	# data of fields is used as an input to Form::Diva
+	# note: name is added automatically from field name
+	form_field 'normal_edit' => (
+		data => {type => 'text'},
+	);
+
+	# passing type => 'hidden' will make the field hidden in diva
+	form_field 'hidden' => (
+		data => {type => 'hidden'},
+	);
+
+	# if there is no data section at all, the field is also treated as hidden
+	# default value in form is used
+	form_field 'also_hidden' => (
+		default => sub { 55 },
+	);
+
+
+	### Using Form::Diva
+
+	my $form = MyFormPackage->new;
+	$form->set_input({normal_edit => 'edited!'});
+
+	# Form::Diva adapter - see Form::Diva docs
+	my $diva = $form->diva;
+
+	# no need to pass the data in first argument to functions that need it
+	print Dumper($form->generated);
 
 =head1 DESCRIPTION
 
-Stub documentation for Form::Tiny::Plugin::Diva, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This plugin adds some HTML outputting capabilities to L<Form::Tiny> forms using L<Form::Diva>.
 
-Blah blah blah.
+=head1 CONFIGURATION
 
-=head2 EXPORT
+This plugin can be added to Form::Tiny with the following line:
 
-None by default.
+	use Form::Tiny plugins => ['Diva'];
 
+=head2 Form::Diva form scope configuration
 
+By default, these values will be passed to Form::Diva constructor:
+
+	id_base => 'form-field-',
+	label_class => 'form-label',
+	input_class => 'form-control',
+	error_class => 'invalid-feedback',
+
+These can be changed by a call to a new C<diva_config> DSL keyword:
+
+	# one or more at a time
+	diva_config
+		label_class => 'form-label',
+		input_class => 'form-control';
+
+=head2 Form::Diva field scope configuration
+
+All fields defined in the form will be passed into Form::Diva constructor:
+
+=over
+
+=item * fields with no C<data> attribute will be used as hidden fields
+
+=item * fields with C<data> attribute must have it as a hash reference and will be used as regular fields, unless C<< type => 'hidden' >> is specified
+
+=back
+
+Contents of C<data> are documented in L<Form::Diva/"form">. A couple of notes:
+
+=over
+
+=item * C<name> will be automatically copied over from Form::Tiny field name, so there's no need to write it out loud
+
+=item * C<default> will be copied over from Form::Tiny default, but only if not explicitly passed
+
+=item * C<comment> is reserved for internal use
+
+=back
+
+=head2 Printing out the form
+
+You can get a preconfigured diva object by calling C<diva> method on your form instance:
+
+	my $diva = $form_instance->diva;
+
+This is a subclass of Form::Diva, and it behaves slightly differently. The main difference is that you no longer need to pass the data explicitly into methods like C<generate> or C<prefill>, as the form input will be used by default:
+
+	# no arguments, yet will use $form_instance->input
+	my $generated = $diva->generated;
+
+Additionally, all generated fields will have an extra hash key, C<errors> which will contain the string with errors ready to be put into HTML:
+
+	my $errors = $generated->[0]{errors};
+
+When there are no errors, this value will be empty (but never undefined). You can reliably use it with your template engine to print errors. Note that no form scope errors are included - for those you will have to call another method C<form_errors> on diva adapter:
+
+	my $global_errors = $diva->form_errors;
+
+You can configure the HTML class of error containers with C<error_class> configuration field (see above).
+
+=head1 LIMITATIONS
+
+=over
+
+=item * No support for nesting - neither nested arrays nor hashes
+
+Form should print without problems, but will not accept the data back from HTML without modification.
+
+=item * Occupies 'comment' field from Form::Diva
+
+Comment field is needed to pass metadata through the generation mechanism. Contents of this field explicitly configured in forms will be discarded.
+
+=back
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+L<Form::Tiny>
 
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<Form::Diva>
 
 =head1 AUTHOR
 
-bartosz, E<lt>bartosz@nonetE<gt>
+Bartosz Jarzyna, E<lt>bbrtj.pro@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2022 by bartosz
+Copyright (C) 2022 by Bartosz Jarzyna
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.34.0 or,
